914362f8c647ae9c49f5f4ffc2d8fe1ac18bb503
Erin Tait <erinmtait@gmail.com>
2019-03-05 11:54:32 -0800

    added offline check for contact form

--- js/main.js
+++ js/main.js
@@ -416,28 +416,56 @@ jQuery(document).ready(function () {
                 comment: {required: true}
             }
         });
-        jQuery(formvalidate).submit(function (e) {			
-            e.preventDefault();
-            if (formvalidate.valid()) {
-                jQuery('#submit').text('SENDING...');
 
-                jQuery.ajax({type: 'post', url: "mail_handler.php", data: jQuery(formvalidate).serialize(), success: function (result) {
-                        var $response = jQuery.parseJSON(result);
-                        jQuery('#submit').text('contact me');
-                        if ($response.success) {
-                            jQuery('.error-msg').remove();
-                            jQuery('.success-msg').remove();
-                            jQuery('<p class="success-msg">' + $response.message + '</p>').insertAfter('#submit');
-                            jQuery(formvalidate[0]).find("input[type=text], textarea, input[type=email]").val("");
-                        } else {
-                            jQuery('.error-msg').remove();
-                            jQuery('.success-msg').remove();
-                            jQuery('<p class="error-msg">' + $response.message + '</p>').insertAfter('#submit');
-                        }
-                    }});
-            }
-            return false;
-        });
+        if(navigator.onLine){
+            jQuery(formvalidate).submit(function (e) {			
+                e.preventDefault();
+                if (formvalidate.valid()) {
+                    jQuery('#submit').text('SENDING...');
+    
+                    jQuery.ajax({type: 'post', url: "mail_handler.php", data: jQuery(formvalidate).serialize(), success: function (result) {
+                            var $response = jQuery.parseJSON(result);
+                            jQuery('#submit').text('contact me');
+                            if ($response.success) {
+                                jQuery('.error-msg').remove();
+                                jQuery('.success-msg').remove();
+                                jQuery('<p class="success-msg">' + $response.message + '</p>').insertAfter('#submit');
+                                jQuery(formvalidate[0]).find("input[type=text], textarea, input[type=email]").val("");
+                            } else {
+                                jQuery('.error-msg').remove();
+                                jQuery('.success-msg').remove();
+                                jQuery('<p class="error-msg">' + $response.message + '</p>').insertAfter('#submit');
+                            }
+                        }});
+                }
+                return false;
+            });
+        } else {
+            jQuery('<p class="error-msg">' + 'ERROR: You are offline.' + '</p>').insertAfter('#submit');
+        }
+
+        // jQuery(formvalidate).submit(function (e) {			
+        //     e.preventDefault();
+        //     if (formvalidate.valid()) {
+        //         jQuery('#submit').text('SENDING...');
+
+        //         jQuery.ajax({type: 'post', url: "mail_handler.php", data: jQuery(formvalidate).serialize(), success: function (result) {
+        //                 var $response = jQuery.parseJSON(result);
+        //                 jQuery('#submit').text('contact me');
+        //                 if ($response.success) {
+        //                     jQuery('.error-msg').remove();
+        //                     jQuery('.success-msg').remove();
+        //                     jQuery('<p class="success-msg">' + $response.message + '</p>').insertAfter('#submit');
+        //                     jQuery(formvalidate[0]).find("input[type=text], textarea, input[type=email]").val("");
+        //                 } else {
+        //                     jQuery('.error-msg').remove();
+        //                     jQuery('.success-msg').remove();
+        //                     jQuery('<p class="error-msg">' + $response.message + '</p>').insertAfter('#submit');
+        //                 }
+        //             }, timeout: 5000});
+        //     }
+        //     return false;
+        // });
     } catch (err) {
         console.log('error', err);
     }
